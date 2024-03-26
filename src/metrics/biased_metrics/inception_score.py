4efa896e3609dec6c3cfb2eeb2c18cd6612ed4a5
PritRaj1 <pr478@cam.ac.uk>
2024-03-26 16:44:06 +0000

    inception score fixed

--- src/metrics/biased_metrics/inception_score.py
+++ src/metrics/biased_metrics/inception_score.py
@@ -5,14 +5,15 @@ import jax
 
 def scan_kl(carry, x):
     """Scan function for KL divergence"""
-    return None, kl_div(log_softmax(x), jax.lax.expand_dims(jnp.mean(x, axis=0), 0))
+    return None, jnp.sum(kl_div(log_softmax(x), jnp.mean(x, axis=0)))
 
+@jax.jit
 def calculate_is(fake_features, splits=10):
     """exp(E_{x ~ p(x)}[KL(p(y|x) || p(y))])"""
 
-    prob = softmax(fake_features)
+    prob = softmax(fake_features, axis=-1)
+    split_indices = [i * (prob.shape[0] // splits) for i in range(splits + 1)]
+    parts = jnp.array_split(prob, split_indices)
+    _, kl_div_stack = jax.lax.scan(scan_kl, None, jnp.array(parts[1:-1]))
 
-    parts = jnp.split(prob, splits)
-    _, kl_div_stack = jax.lax.scan(scan_kl, None, parts)
-
-    return jnp.exp(jnp.sum(kl_div_stack, axis=0) / splits)
+    return jnp.exp(jnp.mean(kl_div_stack, axis=0))
