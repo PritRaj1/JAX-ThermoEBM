4cd37543c23598c84ca4b3363d593c19178370a1
PritRaj1 <pr478@cam.ac.uk>
2024-03-26 16:02:33 +0000

    fixed inception score

---
+++ results/plot_schedules.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import configparser
+import optax
+
+parser = configparser.ConfigParser()
+parser.read("hyperparams.ini")
+
+# Set plot styling
+sns.set(font_scale=1.1)
+sns.set_style("whitegrid", rc ={'text.usetex' : True, 'font.family' : 'serif', 'font.serif' : ['Computer Modern']})
+
+begin = int(parser["LR_SCHEDULE"]["BEGIN_EPOCH"]) * int(parser["PIPELINE"]["NUM_TRAIN_DATA"])/int(parser["PIPELINE"]["BATCH_SIZE"])
+LR_0 = float(parser["OPTIMIZER"]["G_INITIAL_LR"])
+LR_1 = float(parser["OPTIMIZER"]["G_FINAL_LR"])
+GAMMA = float(parser["LR_SCHEDULE"]["DECAY_RATE"])
+STEP = int(parser["LR_SCHEDULE"]["STEP_INTERVAL"]) * int(parser["PIPELINE"]["NUM_TRAIN_DATA"])/int(parser["PIPELINE"]["BATCH_SIZE"])
+
+LR_schedule = optax.exponential_decay(init_value=LR_0, transition_steps=STEP, decay_rate=GAMMA, transition_begin=begin, end_value=LR_1)
+
+epochs = np.linspace(0,int(parser["PIPELINE"]["NUM_EPOCHS"]))
+steps = epochs * int(parser["PIPELINE"]["NUM_TRAIN_DATA"])/int(parser["PIPELINE"]["BATCH_SIZE"])
+
+plt.figure()
+plt.plot(epochs,[LR_schedule(step) for step in steps])
+plt.xlabel("Epoch")
+plt.ylabel("Learning Rate")
+plt.title("Parameter update")
+plt.savefig("results/lr_schedule.png")
+
+p_list = [0, 0.3, 1, 3, 10]
+num_temps = 100
+
+plt.figure(figsize=(10, 6))
+for p in p_list:
+    temp = np.linspace(0, 1, num_temps) ** p
+    label = "p = {}".format(p) if p != 0 else "p = 0 (Vanilla Model)"
+    plt.plot(temp, label=label)
+plt.xlabel("Schedule Index")
+plt.ylabel("Temperature")
+plt.title("Temperature Schedule")
+plt.legend(loc="upper left")
+plt.savefig("results/temperature_schedule.png")
+
--- src/metrics/biased_metrics/inception_score.py
+++ src/metrics/biased_metrics/inception_score.py
@@ -1,13 +1,18 @@
 import jax.numpy as jnp
 from optax import kl_divergence as kl_div
 from flax.linen import softmax, log_softmax
+import jax
 
-def calculate_is(fake_features):
+def scan_kl(carry, x):
+    """Scan function for KL divergence"""
+    return None, kl_div(log_softmax(x), jax.lax.expand_dims(jnp.mean(x, axis=0), 0))
+
+def calculate_is(fake_features, splits=10):
     """exp(E_{x ~ p(x)}[KL(p(y|x) || p(y))])"""
 
     prob = softmax(fake_features)
-    marginal = prob.mean(axis=0)
-    log_conditional = log_softmax(fake_features)
-    kl_divergence = kl_div(log_conditional, marginal)
 
-    return jnp.exp(kl_divergence.mean())
+    parts = jnp.split(prob, splits)
+    _, kl_div_stack = jax.lax.scan(scan_kl, None, parts)
+
+    return jnp.exp(jnp.sum(kl_div_stack, axis=0) / splits)
