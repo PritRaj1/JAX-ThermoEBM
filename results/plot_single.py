985826e8eddcdaeb9cd8fa588cfa87f122a9bf7a
PritRaj1 <pr478@cam.ac.uk>
2024-03-28 11:20:06 +0000

    ran for p=0

--- results/plot_single.py
+++ results/plot_single.py
@@ -7,7 +7,7 @@ df = pd.read_csv("logs/CelebA/p=0/batch=100/experiment0.csv")
 
 # Plot df["Train Loss"]
 plt.figure()
-plt.plot(df["Train Loss"], label="Val")
+plt.plot(df["KID_inf"], label="Val")
 plt.legend()
 plt.xlabel("Epoch")
 plt.ylabel("Train Loss")
