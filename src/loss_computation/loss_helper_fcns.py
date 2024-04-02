fixed the analystic kl bias and normalisation. we are finall ready to go!

src/loss_computation/loss_helper_fcns.py
@@ -5,7 +5,6 @@ import configparser
 
 from src.MCMC_Samplers.sample_distributions import sample_posterior, sample_prior
 from src.MCMC_Samplers.log_pdfs import log_llood_fcn, log_prior_fcn
-from src.MCMC_Samplers.grad_log_probs import sum_llhood
 
 parser = configparser.ConfigParser()
 parser.read("hyperparams.ini")
@@ -35,6 +34,7 @@ def batch_sample_posterior(key, x, t, EBM_params, GEN_params, EBM_fwd, GEN_fwd):
 
 def prior_norm(key, EBM_params, EBM_fwd):
     """Returns the normalisation constant for the prior distribution."""
+
     key, z = sample_prior(key, EBM_params, EBM_fwd)
     return log_prior_fcn(z, EBM_params, EBM_fwd).sum()
 
@@ -42,45 +42,49 @@ def prior_norm(key, EBM_params, EBM_fwd):
 get_priors = jax.vmap(prior_norm, in_axes=(0, None, None))
 
 
-def marginal_llhood(x, z, t, EBM_params, GEN_params, EBM_fwd, GEN_fwd):
+def llhood(z, x, t, GEN_params, GEN_fwd):
+    """Returns the log-likelihood of the generator model for one sample."""
+    
+    llhood = log_llood_fcn(z, x, t, GEN_params, GEN_fwd)
+    llhood -= jax.scipy.special.logsumexp(stop_gradient(llhood)) # Normalisation is not parameter dependent, required to compare TI and Vanilla
+    return llhood.sum() # log[ p_β(x|z,t) ] = log[ p_β(x1|z1,t) ] + log[ p_β(x2|z2,t) ] + ... + log[ p_β(xN|zN,t) ]
+
+
+def joint_dist(x, z, t, EBM_params, GEN_params, EBM_fwd, GEN_fwd):
     """
     Returns log[ p_β(x|z,t) ] + log[ p_α(z) ] - log[ Z_{θ,t} ]
 
-    If t=1 AND z ~ p(z|x,t=1), this is the value of the marginal logpdf.
+    If t=1 AND z ~ p(z|x,t=1), this is the value of the joint logpdf.
     If t>1, this is the value of the posterior logpdf at temperature t for a given z.
-    The normalisation is very costly, but we cannot call stop_gradient on it, since
-    Z_{θ,t} is parameter dependent. Without normalisation, it becomes difficult
-    to compare the marginal logpdfs between the TI and Vanilla losses.
 
-    Likelihood normalisation is not paramter dependent, but is dependent on t, so 
+    Likelihood normalisation is not paramter dependent, but is dependent on t, so
     we apply logsumexp with stop grad. Prior normalisation is conducted outside of this function.
     """
 
-    llhood = log_llood_fcn(z, x, t, GEN_params, GEN_fwd)
+    llhood = llhood(z, x, t, GEN_params, GEN_fwd)
     prior = log_prior_fcn(z, EBM_params, EBM_fwd)
-    llhood -= jax.scipy.special.logsumexp(stop_gradient(llhood))
-
-    return llhood.sum() + prior.sum()
+    return llhood + prior.sum()
 
 
-marginal = jax.vmap(marginal_llhood, in_axes=(0, 0, None, None, None, None, None))
-batched_sum_llhood = jax.vmap(sum_llhood, in_axes=(0, 0, None, None, None))
+joint_logpdf = jax.vmap(joint_dist, in_axes=(0, 0, None, None, None, None, None))
 
 
-def batched_marginal_llhood(key, x, z, t, EBM_params, GEN_params, EBM_fwd, GEN_fwd):
-    logpdf = marginal(x, z, t, EBM_params, GEN_params, EBM_fwd, GEN_fwd)
+def batched_joint_logpdf(key, x, z, t, EBM_params, GEN_params, EBM_fwd, GEN_fwd):
+    logpdf = joint_logpdf(x, z, t, EBM_params, GEN_params, EBM_fwd, GEN_fwd)
 
-    # Prior normalisation is parameter dependent = E_{z~p_α(z)}[ exp(f_α(z)) ], which is easier to track grads for than logsum.
-    keybatch = jax.random.split(key, batch_size+1)
+    # Prior normalisation is parameter dependent = E_{z~p_α(z)}[ exp(f_α(z)) ].
+    keybatch = jax.random.split(key, batch_size + 1)
     key, subkey_batch = keybatch[0], keybatch[1:]
     logpdf -= get_priors(subkey_batch, EBM_params, EBM_fwd).mean()
     return logpdf
 
 
+batched_llhood = jax.vmap(llhood, in_axes=(0, 0, None, None, None))
+
+
 def mean_llhood(x, z, GEN_params, GEN_fwd):
     """Returns the expected normalised log-likelihood of the generator model."""
 
-    # Untempered, therefore exact normalisation constant is feasible.
-    llhood = batched_sum_llhood(z, x, 1, GEN_params, GEN_fwd)
-    llhood -= 0.5 * m * jnp.log(2 * jnp.pi * pl_sigma**2)
+    # Untempered likelihood, E_{z~p_θ(z|x,t)}[ log(p_β(x|z)) ]
+    llhood = batched_llhood(z, x, 1.0, GEN_params, GEN_fwd)
     return llhood.mean()

src/loss_computation/thermo_KL.py
@@ -1,10 +1,6 @@
-import jax
 import jax.numpy as jnp
-from jax.lax import stop_gradient
 from jax.scipy.linalg import det, inv
 import configparser
-
-from src.loss_computation.loss_helper_fcns import batched_marginal_llhood
 
 parser = configparser.ConfigParser()
 parser.read("hyperparams.ini")
@@ -13,7 +9,7 @@ p0_sig = float(parser["SIGMAS"]["p0_SIGMA"])
 z_channels = int(parser["EBM"]["Z_CHANNELS"])
 
 
-def KL_div(z1, z2, eps=1e-8, ridge=1e-5):
+def KL_div(z1, z2, eps=1e-8, ridge=1e-6):
     """Analytic solution for KL Divergence between power posteriors, assuming multivariate Gaussians."""
     m1 = jnp.mean(z1, axis=0)
     m2 = jnp.mean(z2, axis=0)
@@ -23,43 +19,16 @@ def KL_div(z1, z2, eps=1e-8, ridge=1e-5):
     KL_div = 0.5 * (
         jnp.log((det(var2) + eps) / (det(var1) + eps))
         + jnp.trace(inv(var2) @ var1)
-        + (m1 - m2).T @ inv(var2) @ (m1 - m2)
+        + (m2 - m1).T @ inv(var2) @ (m2 - m1)
         - z_channels
     )
 
     return KL_div
 
 
-def analytic_KL_bias(key, z_prev, z_curr, *args):
+def analytic_KL_bias(z_prev, z_curr):
     """Returns the KL divergence bias term between two adjacent temperatures."""
 
     z_prev = z_prev.squeeze()
     z_curr = z_curr.squeeze()
     return KL_div(z_prev, z_curr) - KL_div(z_curr, z_prev)
-
-
-def inferred_KL_bias(
-    key, z_prev, z_curr, t_prev, t_curr, x, EBM_params, GEN_params, EBM_fwd, GEN_fwd
-):
-
-    # p(z|x, t) = p(x|z, t) * p(z|t) / p(x|t)
-    def expected_posterior(prior_key, z, t):
-        llhood = batched_marginal_llhood(prior_key, x, z, t, EBM_params, GEN_params, EBM_fwd, GEN_fwd)
-        llhood -= jax.scipy.special.logsumexp(stop_gradient(llhood))
-        return llhood.mean() # E_{p(z|x,t)}[ log(p(z|x,t)) ]
-
-    keys = jax.random.split(key, 4)
-
-    # Sample z' ~ p(z|x,t_{i-1}) and find the resulting log(p(z'|x,t) evaluations
-    # E_{p(z'|x, t_{i-1})}[ log(p(z|x, t_{i-1})) - log(p(z|x,t_i)) ]
-    KL_high = expected_posterior(prior_key=keys[0], z=z_prev, t=t_prev) - expected_posterior(
-        prior_key=keys[1], z=z_prev, t=t_curr
-    )
-
-    # Sample z'' ~ p(z|x,t_i) and find the resulting log(p(z''|x,t) evaluations
-    # E_{p(z''|t_i)}[ log(p(x|z,t_i)) - log(p(x|z,t_{i-1})) ]
-    KL_low = expected_posterior(prior_key=keys[2], z=z_curr, t=t_curr) - expected_posterior(
-        prior_key=keys[3], z=z_curr, t=t_prev
-    )
-
-    return KL_high - KL_low

src/loss_computation/thermo_computation.py
@@ -5,7 +5,7 @@ from functools import partial
 import configparser
 
 from src.loss_computation.loss_helper_fcns import batch_sample_posterior, mean_llhood
-from src.loss_computation.thermo_KL import analytic_KL_bias, inferred_KL_bias
+from src.loss_computation.thermo_KL import analytic_KL_bias
 
 parser = configparser.ConfigParser()
 parser.read("hyperparams.ini")
@@ -14,15 +14,13 @@ batch_size = int(parser["PIPELINE"]["BATCH_SIZE"])
 z_channels = int(parser["EBM"]["Z_CHANNELS"])
 temp_power = float(parser["TEMP"]["TEMP_POWER"])
 num_temps = int(parser["TEMP"]["NUM_TEMPS"])
-include_bias = str(parser["TEMP"]["INCLUDE_BIAS"])
+include_bias = bool(parser["TEMP"]["INCLUDE_BIAS"])
 
 temp_schedule = jnp.linspace(0, 1, num_temps) ** temp_power
 
 # Determine which bias term to use
-if include_bias == "analytic":
+if include_bias:
     get_bias = analytic_KL_bias
-elif include_bias == "inferred":
-    get_bias = inferred_KL_bias
 else:
     get_bias = lambda *args: 0.0
 
@@ -43,18 +41,8 @@ def thermo_scan_loop(carry, t, x, EBM_params, GEN_params, EBM_fwd, GEN_fwd):
     delta_T = t - t_prev
 
     # ((L(t_i) + L(t_{i-1})) * ∇T) + (KL[z_{i-1} || z_i] - KL[z_i || z_{i-1}])
-    key, subkey = jax.random.split(key)
     temperature_loss = (current_loss + prev_loss) * delta_T + get_bias(
-        subkey,
-        prev_z,
-        z_posterior,
-        t_prev,
-        t,
-        x,
-        EBM_params,
-        GEN_params,
-        EBM_fwd,
-        GEN_fwd,
+        prev_z, z_posterior
     ) * keep_KL  # Do not include KL divergence in first iter, (area is 0 between t=0 and t=0)
 
     # Push tempered loss to the stack and carry over the current state